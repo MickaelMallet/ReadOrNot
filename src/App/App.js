12e446bc14d7435a42b24e3c770dc22b8a63256b
MickaelMallet <mickaelmallet7@gmail.com>
2023-02-23 10:30:53 +0100

    loader

--- src/App/App.js
+++ src/App/App.js
@@ -6,22 +6,39 @@ import Signup from '../components/auth/Signup';
 import Footer from '../components/footer/Index';
 import LoginPage from '../components/auth/Login';
 import LibraryPage from '../components/library';
+import Loader from './Loader';
+import './loaderStyle.scss';
 
 function App() {
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <Router>
       <div className="app-container">
-        <Navbar />
-        <Routes>
-          <Route path="/" element = {<Home />} />
-          <Route path="/signup" element = {<Signup />} />
-          <Route path="/login" element = {<LoginPage />} />
-          <Route path="/bibliotheque" element = {<LibraryPage />} />
-        </Routes>
-        <Footer />
+        {loading ? (
+          <Loader /> // Afficher le loader si la page est en cours de chargement
+        ) : (
+          <>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/bibliotheque" element={<LibraryPage />} />
+            </Routes>
+            <Footer />
+          </>
+        )}
       </div>
     </Router>
   );
 }
 
 export default App;
+
