e41405ad14c3b7e6884fb64a0d51786109377378
Whitney Toniolo <whitney.toniolo@gmail.com>
2023-02-27 15:26:02 +0100

    CSS de la loginPage

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -43,23 +43,45 @@ function LoginPage() {
   };
   
   return (
-    <div className="login-container">
-      <h1 className="login-title">Connexion</h1>
-      <form onSubmit={handleSubmit} className="login-form">
-        <label>
-          Nom d'utilisateur :
-          <input type="text" value={email} onChange={handleEmailChange} />
-        </label>
-        <br />
-        <label>
-          Mot de passe :
-          <input type="password" value={password} onChange={handlePasswordChange} />
-        </label>
-        <br />
-        <input type="submit" value="Se connecter" />
-      </form>
-    </div>
+<div className="container" id="container">
+	<div class="form-container sign-in-container">
+    <form onSubmit={handleSubmit} class="login-form">
+			<h1>Se connecter</h1>
+			<input type="text" value={email} onChange={handleEmailChange} />
+      <input type="password" value={password} onChange={handlePasswordChange} />
+			<input type="submit" value="Se connecter" class="submit-btn" />
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-right">
+				<h1>Hello, Friend!</h1>
+				<p>Enter your personal details and start journey with us</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
   );
 }
 
 export default LoginPage;
+{/* <div class="login-container">
+  <div class="form-container">
+    <h1 class="login-title">Connexion</h1>
+    <form onSubmit={handleSubmit} class="login-form">
+      <div class="input-field">
+        <input type="text" value={email} onChange={handleEmailChange} />
+        <label>Email</label>
+      </div>
+      <div class="input-field">
+        <input type="password" value={password} onChange={handlePasswordChange} />
+        <label>Mot de passe</label>
+      </div>
+      <input type="submit" value="Se connecter" class="submit-btn" />
+    </form>
+  </div>
+</div> */}
