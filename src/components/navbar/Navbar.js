12c0943fc731dffc063194aada62eaf20852f29b
Whitney Toniolo <whitney.toniolo@gmail.com>
2023-03-03 03:03:13 +0100

    css Navbar/centrage du texte

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -9,23 +9,27 @@ function Navbar() {
         {/* <Link to="/" className="navbar-logo">
           LOGO
         </Link> */}
-        <div className="navbar-links">
+        <div className="navbar-content-left">
           <Link to="/bibliotheque" className="navbar-link">
-            Bibliothèque
+            Bibliothèque  
           </Link>
           <Link to="/login" className="navbar-link">
-            Login
+            Login  
           </Link>
           <Link to="/signup" className="navbar-link">
-            Inscription
+            Inscription  
           </Link>
+          </div>
+          <div className="navbar-content-center">
           <a className="navbar-link-logo" href='/'><img src="https://cdn.discordapp.com/attachments/1061603926757085186/1079764403840368722/Logo_ReadOrNotV2.png" alt="logo" /></a>
-
+          </div>
+          <div className="navbar-content-right">
           <Link to ="/mypage" className="navbar-link">
            Profil
           </Link>
+          </div>
         </div>
-      </div>
+
     </div>
   );
 }
