95399c5395a2232ac4d98855257ec052028f3037
Whitney Toniolo <whitney.toniolo@gmail.com>
2023-03-08 10:53:06 +0100

    ajout de livres a la bibli

--- src/Components/Books/BookDetail.js
+++ src/Components/Books/BookDetail.js
@@ -1,11 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import './bookDetail.scss'
 
 function BookDetails({ match }) {
   const [book, setBook] = useState(null);
-
   const { id } = useParams();
+
+  const handleToReadClick = () => {
+    // Mettez à jour la liste "à lire" dans le local storage en ajoutant le livre
+    const toReadList = JSON.parse(localStorage.getItem("toReadList") || "[]");
+    localStorage.setItem(
+      "toReadList",
+      JSON.stringify([...toReadList, book.id])
+    );
+  };
+
+  const handleReadClick = () => {
+    // Mettez à jour la liste "lu" dans le local storage en ajoutant le livre
+    const readList = JSON.parse(localStorage.getItem("readList") || "[]");
+    localStorage.setItem(
+      "readList",
+      JSON.stringify([...readList, book.id])
+    );
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,6 +44,8 @@ function BookDetails({ match }) {
       <p>{book.description}</p>
       <p>Auteur : {book.author.firstname} {book.author.lastname}</p>
       <p>Genre : {book.category.name}</p>
+      <button onClick={handleToReadClick}>A lire</button>
+      <button onClick={handleReadClick}>Lu</button>
     </div>
   );
 }
--- src/Components/Mypage/Mypage.js
+++ src/Components/Mypage/Mypage.js
@@ -4,65 +4,75 @@ import "react-alice-carousel/lib/alice-carousel.css";
 import "./Mypage.scss";
 
 const Mypage = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [data, setData] = useState([]);
+const [currentIndex, setCurrentIndex] = useState(0);
+const [data, setData] = useState([]);
+const [toReadList, setToReadList] = useState([]);
+const [readList, setReadList] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch("http://kilianthoraval-server.eddi.cloud:8080/books");
-      const result = await response.json();
-      setData(result);
-    };
-    
-    fetchData();
-  }, []);
+useEffect(() => {
+const fetchData = async () => {
+const response = await fetch("http://kilianthoraval-server.eddi.cloud:8080/books");
+const result = await response.json();
+setData(result);
+};
 
+fetchData();
 
-  const responsive = {
-    0: { items: 1 },
-    568: { items: 4 },
-    1024: { items: 6 },
-  };
+// Récupérez la liste "à lire" et la liste "lu" depuis le local storage
+const toReadList = JSON.parse(localStorage.getItem("toReadList") || "[]");
+const readList = JSON.parse(localStorage.getItem("readList") || "[]");
 
-  const itemsBookRead = data.map((book) => (
-    <div key={book.id} className="mypage-carousel-item">
-      <img src={book.img} alt={book.title}/>
-    </div>
-  ));
+// Mettez à jour les états "toReadList" et "readList" avec les listes récupérées
+setToReadList(toReadList);
+setReadList(readList);
+}, []);
 
-  const itemsBookToRead = data.map((book) => (
-    <div key={book.id} className="mypage-carousel-item">
-      <img src={book.img} alt={book.title} />
+// Filtrez les livres à partir des listes "à lire" et "lu"
+const booksToRead = data.filter((book) => toReadList.includes(book.id));
+const booksRead = data.filter((book) => readList.includes(book.id));
 
-    </div>
-  ));
+const responsive = {
+0: { items: 1 },
+568: { items: 4 },
+1024: { items: 6 },
+};
 
-  
-  return (
-    <div className="mypage-container">
-      <h1>Ma Bibliothèque</h1>
-      <h2>Livres lus</h2>
-      <AliceCarousel
-        items={itemsBookRead}
-        responsive={responsive}
-        slideToIndex={currentIndex}
-        // onSlideChanged={handleSlideChange}
-        dotsDisabled={true}
-        buttonsDisabled={true}
-      />
-      <h2>Livres à lire</h2>
-      <AliceCarousel
-        items={itemsBookToRead}
-        responsive={responsive}
-        slideToIndex={currentIndex}
-        // onSlideChanged={handleSlideChange}
-        dotsDisabled={true}
-        buttonsDisabled={true}
-      />
-    </div>
-  );
+const itemsBookRead = booksRead.map((book) => (
+<div key={book.id} className="mypage-carousel-item">
+<img src={book.img} alt={book.title}/>
+</div>
+));
+
+const itemsBookToRead = booksToRead.map((book) => (
+<div key={book.id} className="mypage-carousel-item">
+<img src={book.img} alt={book.title} />
+</div>
+));
+
+return (
+<div className="mypage-container">
+<h1>Ma Bibliothèque</h1>
+<h2>Livres lus</h2>
+<AliceCarousel
+     items={itemsBookRead}
+     responsive={responsive}
+     slideToIndex={currentIndex}
+     dotsDisabled={true}
+     buttonsDisabled={true}
+   />
+<h2>Livres à lire</h2>
+<AliceCarousel
+     items={itemsBookToRead}
+     responsive={responsive}
+     slideToIndex={currentIndex}
+     dotsDisabled={true}
+     buttonsDisabled={true}
+   />
+</div>
+);
 };
 
 export default Mypage;
+
 
 
